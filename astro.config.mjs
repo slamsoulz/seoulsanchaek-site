e2c4047fe31f27f1159e6312add86e521c6e6eaf
slamsoulz <slamsoulz@MacBook-Pro.local>
2026-08-18 12:17:52 +0900

    Update site URL to seoulsanchaek-tour.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://seoulsanchaek.netlify.app',
+  site: 'https://seoulsanchaek-tour.netlify.app',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
