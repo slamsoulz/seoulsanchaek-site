465745bec583505e1193c2e32bec19b5664bfa88
slamsoulz <slamsoulz@MacBook-Pro.local>
2026-09-03 19:37:49 +0900

    Use live Netlify site URL in metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://seoul-walk-tours.netlify.app',
+  site: 'https://seoulwalktours.netlify.app',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
